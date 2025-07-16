fc8d452739360634775903d5851bc2310c5cb6a7
박병관 <pbk575@gmail.com>
2025-07-17 01:26:56 +0900

    [CONFIG] esbuild external dynamic import

--- packages/main/esbuild.config.js
+++ packages/main/esbuild.config.js
@@ -1,4 +1,9 @@
 import esbuild from 'esbuild';
+import { readFileSync } from 'fs';
+
+// package.json을 읽어와 dependencies를 external로 동적으로 설정하기
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
+const external = Object.keys(packageJson.dependencies || {});
 
 // TypeScript 파일을 esbuild로 빌드 (ESNext 타겟)
 await esbuild.build({
@@ -11,7 +16,7 @@ await esbuild.build({
   loader: {
     '.ts': 'ts', // TypeScript 파일 로드
   },
-  external: ["googleapis", "gaxios"], // Node.js 내장 모듈을 번들에서 제외
+  external, // Node.js 내장 모듈 및 dependencies를 번들에서 제외
   minify: false, // 디버깅을 위해 minify 비활성화 (선택사항)
   sourcemap: true, // 소스맵 생성 (선택사항)
 }).catch(() => process.exit(1));
