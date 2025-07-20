8ae933f2b8d3efb2de5b89de077c53f2f5289302
박병관 <pbk575@gmail.com>
2025-07-21 00:19:44 +0900

    [ADD] typeParser

--- packages/main/src/core/DML/implements/InsertBuilder.ts
+++ packages/main/src/core/DML/implements/InsertBuilder.ts
@@ -81,12 +81,12 @@ class SettedInsertBuilder<T extends Schema[], SheetName extends T[number]['sheet
 
                 for (const fieldName in schema.fields) {
                     const field = schema.fields[fieldName];
-                    if (field.dataType === 'date' && field.timestampAtCreated) {
-                        const columnIndex = field.columnOrder - 1; // columnOrder는 1부터 시작, 배열 인덱스는 0부터 시작
+                    // if (field.dataType === 'date' && field.timestampAtCreated) {
+                    //     const columnIndex = field.columnOrder - 1; // columnOrder는 1부터 시작, 배열 인덱스는 0부터 시작
 
-                        const now = new Date();
-                        newRow[columnIndex] = this.dateToGoogleSheetsSerial(now);
-                    }
+                    //     const now = new Date();
+                    //     newRow[columnIndex] = this.dateToGoogleSheetsSerial(now); // not working now
+                    // }
                 }
                 return newRow;
             });
